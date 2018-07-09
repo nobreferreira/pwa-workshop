dcf2f1f02253ac275669872c0b3f6d0de06ef379
Ricardo Silva <ricardani@gmail.com>
2018-07-09 17:17:49 +0100

    Add event listeners to the service worker

---
+++ public/sw.js
@@ -0,0 +1,47 @@
+/* eslint no-restricted-globals: 0 */
+
+const GET = 'GET';
+const CACHE_NAME = 'pwa-workshop';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', () => self.clients.claim());
+
+// const cacheFirst = async event => {
+//     const { request } = event;
+//     const cache = await caches.open(CACHE_NAME);
+//     const cachedResponse = await cache.match(request.url);
+//
+//     if (cachedResponse) {
+//         return cachedResponse;
+//     }
+//
+//     try {
+//         const response = await fetch(request.clone());
+//         await cache.put(event.request.url, response.clone());
+//         return response;
+//     } catch (error) {
+//         return error;
+//     }
+// };
+
+const fetchFirst = async event => {
+    const { request } = event;
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(request.url);
+
+    try {
+        const response = await fetch(request.clone());
+        await cache.put(event.request.url, response.clone());
+        return response;
+    } catch (error) {
+        return cachedResponse;
+    }
+};
+
+self.addEventListener('fetch', event => {
+    if (event.request.method === GET) {
+        // event.respondWith(cacheFirst(event));
+        event.respondWith(fetchFirst(event));
+    }
+});
