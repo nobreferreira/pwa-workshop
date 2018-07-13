774aa86ca43c3be383b7d3a5fa308160da7a938f
Domingos Martins <martins16@gmail.com>
2018-07-13 01:57:46 +0100

    add push notifications

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const webpush = require('web-push');
+
+options = {
+    gcmAPIKey: 'gcmServerKey',
+    TTL: 60
+};
+admin.initializeApp();
+
+exports.sendImageNotification = functions.firestore.document('/photos/{photoUid}').onWrite((snap, context) => {
+    const newImagedata = snap.after.data();
+    const tokens = admin
+        .firestore()
+        .collection(`/tokens`)
+        .get()
+        .then(querySnapshot => {
+            const tokens = [];
+            querySnapshot.forEach(doc => {
+                tokens.push(doc.data());
+            });
+            return tokens;
+        })
+        .catch(error => {
+            console.log('Error getting tokens: ', error);
+        });
+
+    return Promise.all([newImagedata, tokens])
+        .then(values => {
+            const [newImagedata, tokens] = values;
+            const badge =
+                'https://firebasestorage.googleapis.com/v0/b/pwa-blip-ws-00.appspot.com/o/' +
+                'icons%2Fblip-icon-192.png?alt=media&token=abc33849-53f9-4199-853f-e603face7368';
+
+            const payload = JSON.stringify({
+                title: 'Check this new photo',
+                body: `Check out this new photo ${newImagedata.photoDesc} from ${newImagedata.author}!`,
+                image: newImagedata.photo,
+                badge,
+                icon: badge
+            });
+            tokens.forEach(token => {
+                webpush.sendNotification(token, payload, options);
+            });
+            return;
+        })
+        .catch(error => {
+            console.log('Error: ', error);
+        });
+});
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,5 +1,55 @@
+function getUploadBody(endpointValue, authValue, p256dhValue) {
+    return {
+        fields: {
+            keys: {
+                mapValue: {
+                    fields: {
+                        p256dh: {
+                            stringValue: p256dhValue
+                        },
+                        auth: {
+                            stringValue: authValue
+                        }
+                    }
+                }
+            },
+            endpoint: {
+                stringValue: endpointValue
+            }
+        }
+    };
+}
+
+const tokensUrl =
+    'https://firestore.googleapis.com/v1beta1/projects/pwa-blip-ws-00/databases/(default)/documents/tokens/';
+
 export default function register() {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('sw.js');
+
+        navigator.serviceWorker.ready
+            .then(serviceWorkerRegistration =>
+                // userVisibleOnly: A boolean indicating that the returned push subscription
+                // will only be used for messages whose effect is made visible to the user.
+                serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true })
+            )
+            .then(
+                pushSubscription => {
+                    const subscription = pushSubscription.toJSON();
+                    const url = tokensUrl.concat(subscription.endpoint.slice(-20));
+                    fetch(url, {
+                        method: 'PATCH',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(
+                            getUploadBody(subscription.endpoint, subscription.keys.auth, subscription.keys.p256dh)
+                        )
+                    }).catch(error => console.error('Subscribing to notifications failure', error));
+                },
+                error =>
+                    console.log(
+                        `User rejected notifications permission or there is a problem with secure origins: ${error}`
+                    )
+            )
+            .catch(error => console.log('Error: ', error));
     }
 }
